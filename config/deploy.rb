8c0d7bc998bc3b477235ea55034b935fd6bcf28e
aya0625-cmd <ayaaya06250625@gmail.com>
2020-06-12 16:56:04 +0900

    w

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '<Capistranoのバージョン>'
+lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, '<自身のアプリケーション名>'
+set :application, 'cake_info_app'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:aya0625-cmd/cake_info_app.git'
